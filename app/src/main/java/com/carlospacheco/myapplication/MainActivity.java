026f3c60872b359436bfcc90a8dbce6c06bd0186
dpguto <carlos.pacheco@aluno.ifsp.edu.br>
2020-08-15 18:13:18 -0300

    Atualização de bibliotecas e tentativa de conexao, display conectado e nao conectado com broker Adafruit

--- app/src/main/java/com/carlospacheco/myapplication/MainActivity.java
+++ app/src/main/java/com/carlospacheco/myapplication/MainActivity.java
@@ -3,14 +3,50 @@ package com.carlospacheco.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    static String    MQTTHOST = "tcp://io.adafruit.com:1883";
+    static String    USERNAME = "meganiqui";
+    static String    PASSWORD = "Needfor2";
+    String topicStr  = "ROTACAO";
+    MqttAndroidClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String clientId = MqttClient.generateClientId();
+        client = new MqttAndroidClient (this.getApplicationContext(), MQTTHOST, clientId);
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName(USERNAME);
+        options.setPassword(PASSWORD.toCharArray());
+
+        try {
+            IMqttToken token = client.connect(options);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Toast.makeText(MainActivity.this, "CONECTADO",Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Toast.makeText(MainActivity.this, "NÃO CONECTADO",Toast.LENGTH_LONG).show();
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
     }
 }
